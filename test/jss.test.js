cb53225601e090d01dc39a9f8d23c8703c39febe
pingjiang <pingjiang1989@gmail.com>
2014-09-10 15:05:40 +0800

    Fix jslint of mocha.

--- test/jss.test.js
+++ test/jss.test.js
@@ -3,10 +3,6 @@
 var assert = require('assert');
 var jss = require('../lib/jss');
 
-var appKey = '49de4df0e7b54348a2f2b18304f5daff';
-var appSecret = '63c44a9c87274e5f8ad2b0577d9c97cd99KbnyPy';
-
-
 /**
  * Test.
  */
